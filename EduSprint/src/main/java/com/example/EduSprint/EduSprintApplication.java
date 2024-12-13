89d94a61649875ab4b4ac3fc1509b24131b32785
matino06FER <matino.milicevic@fer.unizg.hr>
2024-12-13 11:40:31 +0100

    Spring boot project setup with JPA

---
+++ EduSprint/src/main/java/com/example/EduSprint/EduSprintApplication.java
@@ -0,0 +1,13 @@
+package com.example.EduSprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduSprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduSprintApplication.class, args);
+	}
+
+}
---
+++ EduSprint/src/test/java/com/example/EduSprint/EduSprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EduSprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduSprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
