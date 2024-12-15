1e035b8fd5fce2c8cf76c194fa2b972b9a44e077
matino06FER <matino.milicevic@fer.unizg.hr>
2024-12-15 08:28:37 +0100

    Deleted unwanted file

--- EduSprint/src/main/java/com/example/EduSprint/repository/SolvedTaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.EduSprint.repository;
-
-import com.example.EduSprint.entity.SolvedTask;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SolvedTaskRepository extends JpaRepository<SolvedTask, Long> {
-}
